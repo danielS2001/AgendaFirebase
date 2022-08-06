e2814e798fd6c8567d54bec562f490698dbad37a
danielS2001 <saenzdaniel2001@outlook.com>
2022-08-06 15:17:19 -0600

    Primer Commit
    First Commit

---
+++ app/src/main/java/com/example/agendafirebase/Objetos/ReferenciasFirebase.java
@@ -0,0 +1,7 @@
+package com.example.agendafirebase.Objetos;
+
+public class ReferenciasFirebase {
+    final static public String URL_DATABASE = "https://miproyecto-91f2a-default-rtdb.firebaseio.com/";
+    final static public String DATABASE_NAME = "agenda";
+    final static public String TABLE_NAME = "contactos";
+}
